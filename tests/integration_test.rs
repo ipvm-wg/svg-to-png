fa246d41a9ed545c7f15f24ae4180057b1e85da5
Brian Ginsburg <gins@brianginsburg.com>
2024-02-14 10:25:45 -0800

    Fix integration test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,7 +2,7 @@
 fn it_works() {
     let input = include_str!("fixtures/example.svg");
     let expected = include_bytes!("fixtures/expected.png");
-    let actual = svg_to_png::rasterize(input.to_string());
+    let actual = svg_to_png::rasterize(input.to_string()).unwrap();
 
     assert_eq!(actual, expected);
 }
